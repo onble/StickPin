bd89bb28b5ee8146d329c5936fec10d8291ee8cd
“onble” <62949816+onble@users.noreply.github.com>
2025-01-28 19:36:09 +0800

    完成10

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,9 +1,15 @@
-import { _decorator, Component, instantiate, Node, Prefab } from "cc";
+import { _decorator, Component, input, Input, instantiate, Label, Node, Prefab } from "cc";
 import { Pin } from "./Pin";
 const { ccclass, property } = _decorator;
 
 @ccclass("GameManager")
 export class GameManager extends Component {
+    private static _inst: GameManager = null;
+
+    public static get inst() {
+        return this._inst;
+    }
+
     @property(Node)
     p1: Node = null;
     @property(Node)
@@ -17,20 +23,50 @@ export class GameManager extends Component {
     @property
     moveDuiation: number = 0.5;
 
+    protected onLoad(): void {
+        GameManager._inst = this;
+        input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+    }
+
+    @property(Node)
+    circleNode: Node = null;
+
+    @property(Label)
+    scoreLabel: Label = null;
+
+    private curPin: Pin = null;
+    private score: number = 0;
+
     start() {
         this.pinSpawn();
     }
 
     update(deltaTime: number) {}
 
+    onTouchStart() {
+        this.curPin.moveTo(this.p3.position, this.moveDuiation, this.circleNode);
+        this.pinSpawn();
+    }
+
+    updateScore() {
+        this.score++;
+        this.scoreLabel.string = this.score.toString();
+    }
+
     pinSpawn() {
         const pinNode = instantiate(this.pinPrefab);
         this.node.addChild(pinNode);
         pinNode.setPosition(this.p1.position);
 
-        const pin = pinNode.getComponent(Pin);
-        if (pin) {
-            pin.moveTo(this.p2.position, this.moveDuiation);
+        this.curPin = pinNode.getComponent(Pin);
+        if (this.curPin) {
+            this.curPin.moveTo(this.p2.position, this.moveDuiation);
+        } else {
+            console.error("你实例化的针身上没有Pin组件，请检查。");
         }
+    }
+
+    protected onDestroy(): void {
+        input.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
     }
 }
--- assets/scripts/Pin.ts
+++ assets/scripts/Pin.ts
@@ -1,10 +1,23 @@
 import { _decorator, Component, Node, tween, Vec3 } from "cc";
+import { GameManager } from "./GameManager";
 const { ccclass, property } = _decorator;
 
 @ccclass("Pin")
 export class Pin extends Component {
-    moveTo(targetPos: Vec3, duration: number = 1) {
-        console.log("moveTo");
-        tween(this.node).to(duration, { position: targetPos }, { easing: "smooth" }).start();
+    moveTo(targetPos: Vec3, duration: number = 1, parentNode: Node = null) {
+        tween(this.node)
+            .to(duration, { position: targetPos }, { easing: "smooth" })
+            .call(() => {
+                if (parentNode != null) {
+                    const p = this.node.getWorldPosition();
+                    const rotation = this.node.getWorldRotation();
+                    this.node.setParent(parentNode);
+                    this.node.setWorldPosition(p);
+                    this.node.setWorldRotation(rotation);
+
+                    GameManager.inst.updateScore();
+                }
+            })
+            .start();
     }
 }
