33746b654f1df34711dbf50100e5f41ebaf5accb
“onble” <62949816+onble@users.noreply.github.com>
2025-01-29 10:21:07 +0800

    完成13

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,4 +1,17 @@
-import { _decorator, Component, input, Input, instantiate, Label, Node, Prefab } from "cc";
+import {
+    _decorator,
+    Camera,
+    Color,
+    Component,
+    director,
+    input,
+    Input,
+    instantiate,
+    Label,
+    Node,
+    Prefab,
+    tween,
+} from "cc";
 import { Pin } from "./Pin";
 import { Circle } from "./Circle";
 const { ccclass, property } = _decorator;
@@ -35,15 +48,32 @@ export class GameManager extends Component {
     @property(Label)
     scoreLabel: Label = null;
 
+    @property(Camera)
+    camera: Camera = null;
+
+    @property
+    targetOrthoHeight: number = 500;
+
+    @property(Color)
+    targetColor: Color = new Color();
+
     private curPin: Pin = null;
     private score: number = 0;
     private isGameOver: boolean = false;
+    private isColorLerp: boolean = false;
 
     start() {
         this.pinSpawn();
     }
 
-    update(deltaTime: number) {}
+    update(deltaTime: number) {
+        if (this.isColorLerp) {
+            let outColor = new Color();
+            Color.lerp(outColor, this.camera.clearColor, this.targetColor, deltaTime * 2);
+
+            this.camera.clearColor = outColor;
+        }
+    }
 
     onTouchStart() {
         if (this.isGameOver) return;
@@ -75,6 +105,13 @@ export class GameManager extends Component {
         this.isGameOver = true;
 
         this.circleNode.getComponent(Circle).stopRotate();
+
+        tween(this.camera).to(0.5, { orthoHeight: this.targetOrthoHeight }, { easing: "smooth" }).start();
+        this.isColorLerp = true;
+
+        this.scheduleOnce(() => {
+            director.loadScene(director.getScene().name);
+        }, 2);
     }
 
     protected onDestroy(): void {
